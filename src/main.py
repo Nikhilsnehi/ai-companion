4c1897433bf0fedb7e0d5f2f1f01050db162f374
Nikhilsnehi <133762377+Nikhilsnehi@users.noreply.github.com>
2026-09-24 23:55:57 +0530

    just commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import time
 from ai import ask_ai, ask_about_screen
 from screen import capture_screen
+
 
 print("AI Companion is running.")
 
@@ -13,9 +15,10 @@ while True:
         break
 
     if any(word in question.lower() for word in screen_words):
+        print("Switch to the window you want me to see...")
+        time.sleep(3)
         capture_screen()
         answer = ask_about_screen(question)
     else:
         answer = ask_ai(question)
-
     print("AI:", answer)
